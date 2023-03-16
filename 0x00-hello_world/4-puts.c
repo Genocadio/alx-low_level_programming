519dc28e54cdc64b380402f751a4b2f97493237d
geno <yvesgeno@outlook.com>
2023-03-16 22:39:20 +0200

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - the function that will print a line
+ * Return: 0 means success
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
