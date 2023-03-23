4f30de6fe112b59c3b8d545f44da2ed787bf3b51
geno <yvesgeno@outlook.com>
2023-03-23 19:40:36 +0200

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - print number between 0 and 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	for (c = '0'; c <= '9'; c++)
+{
+	_putchar(c);
+}
+	_putchar("\n");
+}
