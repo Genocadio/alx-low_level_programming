c344b0f386652ae32bdbeafb8a48a8145609d728
geno <yvesgeno@outlook.com>
2023-03-23 21:48:51 +0200

    correction for task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
- * print_most_numbers - a function that prints the numbers, from 0 to 9
- * @c: char to be used
- * Return: none
+ * print_most_numbers - print num except 2 and 4
+ *
+ * return: void
  */
+
 void print_most_numbers(void)
 {
 	char c;
