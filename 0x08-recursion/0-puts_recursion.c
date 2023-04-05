71e10aca4dd0e58e5f532e5b3c9264db75bb4ab0
geno <yvesgeno@outlook.com>
2023-04-05 22:44:09 +0200

    task 0 correction

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,6 +2,7 @@
 /**
  *_puts_recursion - a function that prints a string followed by a new line
  *Return: no return for this function
+ *@s: string to be printed
  */
 void _puts_recursion(char *s)
 {
