157a74847bd35ffdaeee1d31df87a7617fd02a74
geno <yvesgeno@outlook.com>
2023-04-05 23:38:52 +0200

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,8 +1,15 @@
 #include "main.h"
+/**
+ * _strlen_recursion - function to print length of string
+ * @s: string to be counted
+ * @i: length of string
+ * Return: i the length of string
+ */
 
 int _strlen_recursion(char *s)
 {
 	int i;
+
 	if (*s != '\0')
 	i = 1 + _strlen_recursion(s + 1);
 	return (i);
