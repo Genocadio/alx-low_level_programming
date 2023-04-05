ef293646e67f333ff6c21ea3a59437e34deb1983
geno <yvesgeno@outlook.com>
2023-04-05 23:45:28 +0200

    task 2 correction

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,15 +2,17 @@
 /**
  * _strlen_recursion - function to print length of string
  * @s: string to be counted
- * @i: length of string
  * Return: i the length of string
  */
 
 int _strlen_recursion(char *s)
 {
-	int i;
+	int i = 0;
 
 	if (*s != '\0')
-	i = 1 + _strlen_recursion(s + 1);
+	{
+		i++;
+		i +=  _strlen_recursion(s + 1);
+	}
 	return (i);
 }
