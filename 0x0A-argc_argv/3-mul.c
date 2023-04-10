cd8bb413b98f6eeb0f66b7b4d20a87629e7d1915
geno <yvesgeno@outlook.com>
2023-04-10 22:36:36 +0200

    correction

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,28 +7,41 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1, i = 0, num = 0;
 
-	if (s[i] == '-')
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+	while (i < len && f == 0)
 	{
-		sign = -1;
-		i++;
-	}
-	while (s[i] != '\0')
-	{
+		if (s[i] == '-')
+			++d;
 		if (s[i] >= '0' && s[i] <= '9')
-	{
-			num = num * 10 + (s[i] - '0');
-	}
-		else
-	{
-			break;
-	}
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
 		i++;
 	}
+	if (f == 0)
 
-		return (sign * num);
-	}
+		return (0);
+	return (n);
+
+}
 
 /**
  * main - entry point for program that multiplies two numbers
@@ -47,13 +60,13 @@ int main(int argc, char *argv[])
 		printf("error\n");
 		return (1);
 	}
-	
-	
+
+
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	j = num1 * num2;
 	printf("%d\n", j);
-	
+
 	return (0);
 	}
 
