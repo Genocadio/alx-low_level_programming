dcaa5a21bc7d674cfc02a1689cb137f1d69ca5fc
geno <yvesgeno@outlook.com>
2023-04-10 22:39:43 +0200

    correction

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,49 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * _atoi - function
- * @s: charcter
- * Return: integer value
- */
-int _atoi(char *s)
-{
-
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-	if (f == 0)
-
-		return (0);
-	return (n);
-
-}
-
-/**
  * main - entry point for program that multiplies two numbers
  * @argc: argument count
  * @argv: argument vector
@@ -62,8 +19,8 @@ int main(int argc, char *argv[])
 	}
 
 
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 	j = num1 * num2;
 	printf("%d\n", j);
 
