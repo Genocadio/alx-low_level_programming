6bf23cba4af844d76ff93de26d46e5427140956c
geno <yvesgeno@outlook.com>
2023-04-10 22:11:00 +0200

    correction

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -6,27 +6,27 @@
  */
 int _atoi(char *s)
 {
-    int sign = 1, i = 0, num = 0;
+	int sign = 1, i = 0, num = 0;
 
-  
-    if (s[i] == '-')
-    {
-        sign = -1;
-        i++;
-    }
-    while (s[i] != '\0')
-    {
-        if (s[i] >= '0' && s[i] <= '9')
-        {
-            num = num * 10 + (s[i] - '0');
-        }
-        else
-        {
-            break;
-        }
-        i++;
-    }
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while (s[i] != '\0')
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+	{
+			num = num * 10 + (s[i] - '0');
+	}
+		else
+	{
+			break;
+	}
+		i++;
+	}
 
-    return sign * num;
-}
+		return (sign * num);
+	}
+
 
