0a826cb28392310e31716fb944fcdcd1c6f96b6f
geno <yvesgeno@outlook.com>
2023-04-10 19:48:13 +0200

    task 0 correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
  * main - program entry point for program to print its file name
- * @i: iterator
+ * @argc: argument count to function main
+ * @argv: argument list
  * Return: 0 shows program success
  */
 int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
