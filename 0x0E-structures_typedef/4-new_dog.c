d79b171b12e49d796590ee052d7ac23619c90787
geno <yvesgeno@outlook.com>
2023-04-17 22:56:20 +0200

    correction

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -63,11 +63,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->name = malloc(sizeof(char) * (i + 1));
 	if (dog->name == NULL)
 	{
+		free(dog);
 		return (NULL);
 	}
 	dog->owner = malloc(sizeof(char) * (j + 1));
 	if (dog->owner == NULL)
 	{
+		free(dog);
+		free(dog->name);
 		return (NULL);
 	}
 	_strcpy(dog->name, name);
