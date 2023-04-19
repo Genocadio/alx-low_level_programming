a15a4380a7f2f04d1fbcd20daa30a77eed4ccbbc
geno <yvesgeno@outlook.com>
2023-04-19 19:29:59 +0200

    correction

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,9 @@
 #include "function_pointers.h"
-#include <stdio.h>
 /**
  * print_name - function that prints name
  * @name: name to print
  * @f: function passed to print name
- * Return: void
+ * Return: voidf
  */
 void print_name(char *name, void (*f)(char *))
 {
