8d2ba426763fb16bb7bf60ed9e31a13bef7a0ac7
geno <yvesgeno@outlook.com>
2023-04-27 21:56:59 +0200

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print - function to print a message
+ */
+int print(void) __attribute__((constructor));
+int print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+	return (0);
+}
