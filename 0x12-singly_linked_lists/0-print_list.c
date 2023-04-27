6ee792b9d4712353d6123ed48b99107ce0865d37
geno <yvesgeno@outlook.com>
2023-04-27 20:05:46 +0200

    correction

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,13 @@ size_t print_list(const list_t *h)
 {
 	size_t i = 0;
 
-	while (h != NULL)
+	for (; h != NULL; i++)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		i++;
 	}
 	return (i);
 }
