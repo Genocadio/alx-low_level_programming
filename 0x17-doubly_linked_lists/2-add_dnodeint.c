120d28d1f197d58860c732b23caeb6f1946aab5d
geno <yvesgeno@outlook.com>
2023-06-15 21:00:09 +0200

    correction

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,10 +11,22 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
+	{
 		return (NULL);
+	}
+
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+	}
+	else
+	{
+		new->prev = *head;
+	}
+
 	new->n = n;
-	new->prev = NULL;
 	new->next = *head;
 	*head = new;
+
 	return (new);
 }
